08512bb4be2311c237b62409b4dfdd123751733a
eldolopes <eldolopes@gmail.com>
2020-07-29 20:08:30 -0300

    up

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 
-app.use(express.static('public'))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) =>  {
     res.render('home')
 })
+
+app.get('/home', (req, res) =>  {
+    res.render('home')
+})
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`Server online: http://localhost:${port}`)})
